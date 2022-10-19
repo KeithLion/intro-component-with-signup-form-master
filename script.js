ca5de7ba50a4936808c1f624562636171df890d4
KeithLion <nortonkiara6@gmail.com>
2022-10-19 08:40:54 -0700

    started working on making the site responsive

--- script.js
+++
@@ -1,8 +0,0 @@
-function formValidation() {
-let x = document.getElementById('firstN').value;
-let text;
-if (x==''){
-    text= 'Enter your first name.';
-}
-document.getElementById('validate').innerHTML = text
-}
